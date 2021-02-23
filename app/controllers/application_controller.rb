e9ba4ca4f6ce68d9cd2f94beaecf00b3effc00b5
figo5530 <figooooo0221@gmail.com>
2021-02-23 17:43:59 -0500

    commit tree 11386eaaee0f7997c9a53608dc3bfcaa2e42f199

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,27 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
 
+    post '/teams' do
+        # binding.pry
+        @team_name = params[:team][:name]
+        @team_motto = params[:team][:motto]
+        @hero_name = []
+        @hero_power = []
+        @hero_bio = []
+        @hero_members =params[:team][:members]
+        @hero_members.each do |attri|
+            @hero_name << attri[:name]
+            @hero_power << attri[:power]
+            @hero_bio << attri[:bio]
+        end
+        erb :team
+    end
 end
